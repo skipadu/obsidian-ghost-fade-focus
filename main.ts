808548475f9b76006f0bffe8b63ae936c92d5446
Sami Korpela <skipadu@users.noreply.github.com>
2021-06-27 22:03:14 +0300

    Add some drafting for styles and debugging the missing focus level 1

--- main.ts
+++ main.ts
@@ -16,6 +16,7 @@ const setState = (key: PluginStateKey, value: PluginStateValue) => {
 }
 
 const getDistanceToCurrentLine = (index: number): number => {
+	// TODO: Use Math.abs to simpligy this
 	if (index < pluginState.currentLine) {
 		return pluginState.currentLine - index;
 	} else if (index > pluginState.currentLine) {
@@ -33,6 +34,7 @@ export default class GhostFocusPlugin extends Plugin {
 	}
 	onCursorActivity(doc: CodeMirror.Doc) {
 		const currentCursorPos = doc.getCursor();
+		doc.getCursor().sticky
 		if (pluginState.currentLine !== currentCursorPos.line) {
 			setState('currentLine', currentCursorPos.line);
 			console.log("Current cursorPosition line:", currentCursorPos.line);
@@ -45,7 +47,15 @@ export default class GhostFocusPlugin extends Plugin {
 					if (child.className.match(ghostFocusClassName)) {
 						child.className = child.className.replace(ghostFocusClassName, '');
 					}
-					child.className = child.className.concat(`ghost-focus-${getDistanceToCurrentLine(i)}`);
+					// child.className = child.className.concat(`ghost-focus-${getDistanceToCurrentLine(i)}`);
+					const distance = getDistanceToCurrentLine(i);
+					if (distance <= 5) {
+						// child.className = child.className.concat(`ghost-focus-${distance}`);
+						child.classList.add(`ghost-focus-${distance}`);
+					}
+					// else {
+					// 	child.className = child.className.concat(' ghost-focus');
+					// }
 				}
 			}
 		}
