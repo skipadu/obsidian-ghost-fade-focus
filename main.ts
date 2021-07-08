ea17e2ec8a1d08ef3d6ca81092b98b259b9fd216
Sami Korpela <skipadu@users.noreply.github.com>
2021-07-08 21:23:59 +0300

    Change the logic for removing and adding class names

--- main.ts
+++ main.ts
@@ -2,6 +2,7 @@ import { Plugin } from 'obsidian';
 
 interface State {
 	currentLine?: number;
+	pluginEnabled?: boolean;
 }
 type ValueOf<T> = T[keyof T];
 type PluginStateKey = keyof State;
@@ -15,43 +16,109 @@ const setState = (key: PluginStateKey, value: PluginStateValue) => {
 	}
 }
 
-const getDistanceToCurrentLine = (index: number): number => {
-	// TODO: Use Math.abs to simpligy this
-	if (index < pluginState.currentLine) {
-		return pluginState.currentLine - index;
-	} else if (index > pluginState.currentLine) {
-		return index - pluginState.currentLine;
-	}
-	return 0;
-}
 export default class GhostFocusPlugin extends Plugin {
 	async onload() {
-		pluginState = { currentLine: -1 };
+		pluginState = { currentLine: -1, pluginEnabled: true };
 		console.log('Loading GhostFocusPlugin');
 		this.registerCodeMirror((cm: CodeMirror.Editor) => {
-			cm.on("update", this.onUpdate);
+			cm.on("cursorActivity", this.onCursorActivity);
 		})
 	}
-	onUpdate(doc: CodeMirror.Doc) {
-		const currentCursorPos = doc.getCursor();
-		if (pluginState.currentLine !== currentCursorPos.line) {
-			setState('currentLine', currentCursorPos.line);
-			console.log("Current cursorPosition line:", currentCursorPos.line);
-			const codeMirrorCodeElements = document.getElementsByClassName("CodeMirror-code");
-			const codeMirrorCodeElement = codeMirrorCodeElements !== null && codeMirrorCodeElements.length > 0 ? codeMirrorCodeElements[0] : null;
-			if (codeMirrorCodeElement) {
-				const ghostFocusClassName = new RegExp(/\bghost-focus-.+?\b/, 'g');
-				for (let i = 0; i < codeMirrorCodeElement.children.length; i++) {
-					const child = codeMirrorCodeElement.children[i];
-					if (child.className.match(ghostFocusClassName)) {
-						child.className = child.className.replace(ghostFocusClassName, '');
+
+	onCursorActivity(cm: CodeMirror.Editor) {
+		const addGhostFadeFocusClassNames = (cm: CodeMirror.Editor, totalLines: number, currentCursorPosLine: number) => {
+			for (let i = -5; i <= 5; i++) {
+				const lineNumber = currentCursorPosLine + i;
+				if (lineNumber >= 0 && lineNumber < totalLines) {
+					if (i === 0) {
+						cm.addLineClass(lineNumber, "wrap", 'CodeMirror-activeline');
+					} else {
+						if (pluginState.pluginEnabled) {
+							cm.addLineClass(lineNumber, "wrap", `ghost-fade-focus--${Math.abs(i)}`);
+						}
 					}
-					const distance = getDistanceToCurrentLine(i);
-					if (distance > 0 && distance <= 5) {
-						child.classList.add(`ghost-focus-${distance}`);
+				}
+			}
+			for (let i = 0; i < totalLines; i++) {
+				if (i !== currentCursorPosLine) {
+					cm.addLineClass(i, "wrap", 'ghost-fade-focus');
+				}
+			}
+		};
+
+
+		const removeGhostFadeFocusClassNames = (cm: CodeMirror.Editor, totalLines: number, currentCursorPosLine: number) => {
+			for (let i = 0; i < totalLines; i++) {
+				cm.removeLineClass(i, "wrap");
+				if (i === currentCursorPosLine) {
+					cm.addLineClass(i, "wrap", 'CodeMirror-activeline');
+				}
+			}
+		};
+
+		// TODO: able to toggle the state of the plugin...
+		if (pluginState.pluginEnabled) {
+			const currentCursorPos = cm.getDoc().getCursor();
+			if (pluginState.currentLine !== currentCursorPos.line) {
+				setState('currentLine', currentCursorPos.line);
+				const totalLines = cm.lineCount();
+				removeGhostFadeFocusClassNames(cm, totalLines, currentCursorPos.line);
+				addGhostFadeFocusClassNames(cm, totalLines, currentCursorPos.line);
+			}
+		}
+	}
+
+	addGhostFadeFocusClassNames(cm: CodeMirror.Editor, totalLines: number, currentCursorPosLine: number) {
+		for (let i = -5; i <= 5; i++) {
+			const lineNumber = currentCursorPosLine + i;
+			if (lineNumber >= 0 && lineNumber < totalLines) {
+				if (i === 0) {
+					cm.addLineClass(lineNumber, "wrap", 'CodeMirror-activeline');
+				} else {
+					if (pluginState.pluginEnabled) {
+						cm.addLineClass(lineNumber, "wrap", `ghost-fade-focus--${Math.abs(i)}`);
 					}
 				}
 			}
 		}
-	}
+		for (let i = 0; i < totalLines; i++) {
+			if (i !== currentCursorPosLine) {
+				cm.addLineClass(i, "wrap", 'ghost-fade-focus');
+			}
+		}
+	};
+
+	removeGhostFadeFocusClassNames(cm: CodeMirror.Editor, totalLines: number, currentCursorPosLine: number) {
+		for (let i = 0; i < totalLines; i++) {
+			cm.removeLineClass(i, "wrap");
+			if (i === currentCursorPosLine) {
+				cm.addLineClass(i, "wrap", 'CodeMirror-activeline');
+			}
+		}
+	};
+
+
+	// TODO: onunload() and cm.off("update") etc
+	// onUpdate(doc: CodeMirror.Doc) {
+	// 	const currentCursorPos = doc.getCursor();
+	// 	if (pluginState.currentLine !== currentCursorPos.line) {
+	// 		setState('currentLine', currentCursorPos.line);
+	// 		console.log("Current cursorPosition line:", currentCursorPos.line);
+	// 		const codeMirrorCodeElements = document.getElementsByClassName("CodeMirror-code");
+	// 		const codeMirrorCodeElement = codeMirrorCodeElements !== null && codeMirrorCodeElements.length > 0 ? codeMirrorCodeElements[0] : null;
+	// 		if (codeMirrorCodeElement) {
+	// 			const ghostFocusClassName = new RegExp(/\bghost-focus-.+?\b/, 'g');
+	// 			for (let i = 0; i < codeMirrorCodeElement.children.length; i++) {
+	// 				const child = codeMirrorCodeElement.children[i];
+	// 				if (child.className.match(ghostFocusClassName)) {
+	// 					child.className = child.className.replace(ghostFocusClassName, '');
+	// 				}
+	// 				const distance = getDistanceToCurrentLine(i);
+	// 				if (distance > 0 && distance <= 5) {
+	// 					child.classList.add(`ghost-focus-${distance}`);
+	// 				}
+	// 			}
+	// 		}
+	// 	}
+	// }
 }
