1c91e8dc22ad8c2dc739462dee5d0978123245e4
Sami Korpela <skipadu@users.noreply.github.com>
2021-10-09 13:41:48 +0300

    Add settings for opacity levels
    User can now change the opacity levels and not to rely on hardcoded
    defaults.

--- src/main.ts
+++ src/main.ts
@@ -12,14 +12,27 @@ const setState = (key: PluginStateKey, value: PluginStateValue) => {
 
 interface GhostFocusSettings {
   enabled: boolean;
+  opacity_1: number;
+  opacity_2: number;
+  opacity_3: number;
+  opacity_4: number;
+  opacity_5: number;
+  opacity: number;
 }
 
 const DEFAULT_SETTINGS: Partial<GhostFocusSettings> = {
   enabled: false,
+  opacity_1: 0.85,
+  opacity_2: 0.7,
+  opacity_3: 0.55,
+  opacity_4: 0.4,
+  opacity_5: 0.25,
+  opacity: 0.1,
 };
 
 export default class GhostFocusPlugin extends Plugin {
   settings: GhostFocusSettings;
+  rootElement: HTMLElement;
 
   async loadSettings() {
     this.settings = Object.assign(DEFAULT_SETTINGS, await this.loadData());
@@ -33,6 +46,16 @@ export default class GhostFocusPlugin extends Plugin {
     if (this.settings.enabled) {
       this.addGhostFadeFocusClassNamesToCMs();
     }
+  }
+
+  addCSSVariables() {
+    this.rootElement = document.documentElement;
+    this.rootElement.style.setProperty('--ghost-fade-focus-opacity-1', `${this.settings.opacity_1}`);
+    this.rootElement.style.setProperty('--ghost-fade-focus-opacity-2', `${this.settings.opacity_2}`);
+    this.rootElement.style.setProperty('--ghost-fade-focus-opacity-3', `${this.settings.opacity_3}`);
+    this.rootElement.style.setProperty('--ghost-fade-focus-opacity-4', `${this.settings.opacity_4}`);
+    this.rootElement.style.setProperty('--ghost-fade-focus-opacity-5', `${this.settings.opacity_5}`);
+    this.rootElement.style.setProperty('--ghost-fade-focus-opacity', `${this.settings.opacity}`);
   }
 
   async onload() {
@@ -69,6 +92,8 @@ export default class GhostFocusPlugin extends Plugin {
     this.registerCodeMirror((cm: CodeMirror.Editor) => {
       cm.on("cursorActivity", this.onCursorActivity);
     });
+
+    this.addCSSVariables();
   }
 
   onCursorActivity = (cm: CodeMirror.Editor) => {
@@ -129,10 +154,21 @@ export default class GhostFocusPlugin extends Plugin {
     }
   }
 
+  removeCSSVariables() {
+    this.rootElement = document.documentElement;
+    this.rootElement.style.removeProperty('--ghost-fade-focus-opacity-1');
+    this.rootElement.style.removeProperty('--ghost-fade-focus-opacity-2');
+    this.rootElement.style.removeProperty('--ghost-fade-focus-opacity-3');
+    this.rootElement.style.removeProperty('--ghost-fade-focus-opacity-4');
+    this.rootElement.style.removeProperty('--ghost-fade-focus-opacity-5');
+    this.rootElement.style.removeProperty('--ghost-fade-focus-opacity');
+  }
+
   onunload() {
     this.app.workspace.iterateCodeMirrors((cm: CodeMirror.Editor) => {
       cm.off("cursorActivity", this.onCursorActivity);
       this.removeGhostFadeFocusClassNames(cm);
     });
+    this.removeCSSVariables();
   }
 }
--- src/settings.ts
+++ src/settings.ts
@@ -14,6 +14,8 @@ export class GhostFocusSettingTab extends PluginSettingTab {
 
     containerEl.empty();
 
+    this.containerEl.createEl('h1', { text: 'Ghost Fade Focus settings' });
+
     new Setting(containerEl)
       .setName("Enable Ghost Fade Focus")
       .setDesc(
@@ -32,5 +34,128 @@ export class GhostFocusSettingTab extends PluginSettingTab {
             }
           })
       );
+
+    this.containerEl.createEl('h2', { text: 'Opacity' });
+    this.containerEl.createEl('p', { text: 'These will change the opacity levels used.' });
+
+    let opacityLevel1: HTMLDivElement;
+    new Setting(containerEl)
+      .setName("Opacity - level 1")
+      .setDesc("(Default 0.85)")
+      .addSlider(slider => slider
+        .setLimits(0.1, 0.9, 0.05)
+        .setValue(this.plugin.settings.opacity_1)
+        .onChange(async (value) => {
+          opacityLevel1.innerText = " " + value.toString();
+          this.plugin.settings.opacity_1 = value;
+          this.plugin.rootElement.style.setProperty('--ghost-fade-focus-opacity-1', `${value}`);
+          await this.plugin.saveSettings();
+        }))
+      .settingEl.createDiv('', (el: HTMLDivElement) => {
+        opacityLevel1 = el;
+        el.style.minWidth = "2.0em";
+        el.style.textAlign = "right";
+        el.innerText = " " + this.plugin.settings.opacity_1.toString();
+      });
+
+    let opacityLevel2: HTMLDivElement;
+    new Setting(containerEl)
+      .setName("Opacity - level 2")
+      .setDesc("(Default 0.7)")
+      .addSlider(slider => slider
+        .setLimits(0.1, 0.9, 0.05)
+        .setValue(this.plugin.settings.opacity_2)
+        .onChange(async (value) => {
+          opacityLevel2.innerText = " " + value.toString();
+          this.plugin.settings.opacity_2 = value;
+          this.plugin.rootElement.style.setProperty('--ghost-fade-focus-opacity-2', `${value}`);
+          await this.plugin.saveSettings();
+        }))
+      .settingEl.createDiv('', (el: HTMLDivElement) => {
+        opacityLevel2 = el;
+        el.style.minWidth = "2.0em";
+        el.style.textAlign = "right";
+        el.innerText = " " + this.plugin.settings.opacity_2.toString();
+      });
+
+    let opacityLevel3: HTMLDivElement;
+    new Setting(containerEl)
+      .setName("Opacity - level 3")
+      .setDesc("(Default 0.55)")
+      .addSlider(slider => slider
+        .setLimits(0.1, 0.9, 0.05)
+        .setValue(this.plugin.settings.opacity_3)
+        .onChange(async (value) => {
+          opacityLevel3.innerText = " " + value.toString();
+          this.plugin.settings.opacity_3 = value;
+          this.plugin.rootElement.style.setProperty('--ghost-fade-focus-opacity-3', `${value}`);
+          await this.plugin.saveSettings();
+        }))
+      .settingEl.createDiv('', (el: HTMLDivElement) => {
+        opacityLevel3 = el;
+        el.style.minWidth = "2.0em";
+        el.style.textAlign = "right";
+        el.innerText = " " + this.plugin.settings.opacity_3.toString();
+      });
+
+    let opacityLevel4: HTMLDivElement;
+    new Setting(containerEl)
+      .setName("Opacity - level 4")
+      .setDesc("(Default 0.4)")
+      .addSlider(slider => slider
+        .setLimits(0.1, 0.9, 0.05)
+        .setValue(this.plugin.settings.opacity_4)
+        .onChange(async (value) => {
+          opacityLevel4.innerText = " " + value.toString();
+          this.plugin.settings.opacity_4 = value;
+          this.plugin.rootElement.style.setProperty('--ghost-fade-focus-opacity-4', `${value}`);
+          await this.plugin.saveSettings();
+        }))
+      .settingEl.createDiv('', (el: HTMLDivElement) => {
+        opacityLevel4 = el;
+        el.style.minWidth = "2.0em";
+        el.style.textAlign = "right";
+        el.innerText = " " + this.plugin.settings.opacity_4.toString();
+      });
+
+    let opacityLevel5: HTMLDivElement;
+    new Setting(containerEl)
+      .setName("Opacity - level 5")
+      .setDesc("(Default 0.25)")
+      .addSlider(slider => slider
+        .setLimits(0.1, 0.9, 0.05)
+        .setValue(this.plugin.settings.opacity_5)
+        .onChange(async (value) => {
+          opacityLevel5.innerText = " " + value.toString();
+          this.plugin.settings.opacity_5 = value;
+          this.plugin.rootElement.style.setProperty('--ghost-fade-focus-opacity-5', `${value}`);
+          await this.plugin.saveSettings();
+        }))
+      .settingEl.createDiv('', (el: HTMLDivElement) => {
+        opacityLevel5 = el;
+        el.style.minWidth = "2.0em";
+        el.style.textAlign = "right";
+        el.innerText = " " + this.plugin.settings.opacity_5.toString();
+      });
+
+    let opacityLevel: HTMLDivElement;
+    new Setting(containerEl)
+      .setName("Opacity level outside of 5 steps")
+      .setDesc("(Default 0.1)")
+      .addSlider(slider => slider
+        .setLimits(0.1, 0.9, 0.05)
+        .setValue(this.plugin.settings.opacity)
+        .onChange(async (value) => {
+          opacityLevel.innerText = " " + value.toString();
+          this.plugin.settings.opacity = value;
+          this.plugin.rootElement.style.setProperty('--ghost-fade-focus-opacity', `${value}`);
+          await this.plugin.saveSettings();
+        }))
+      .settingEl.createDiv('', (el: HTMLDivElement) => {
+        opacityLevel = el;
+        el.style.minWidth = "2.0em";
+        el.style.textAlign = "right";
+        el.innerText = " " + this.plugin.settings.opacity.toString();
+      });
   }
 }
